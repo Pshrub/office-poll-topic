3454962823bdd760ff651980ddabe439d3b5e184
pittfagan <pittfagan@gmail.com>
2014-04-17 17:03:43 -0500

    setting up the css hooks for the app

--- office_poll_topic/settings/base.py
+++ office_poll_topic/settings/base.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -81,3 +80,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), "static"), )
